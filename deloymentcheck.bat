627d0eefa9239a3a26ffdbf0f82c37e3c885e070
demouser1 <user1@demo.com>
2022-02-01 10:26:38 +0000

    first commit

---
+++ deloymentcheck.bat
@@ -0,0 +1,2 @@
+kubectl -n bank-apps delete -f spring-node-angular-jwt-k8-deployment.yaml
+exit 0
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  backendServiceUrl: 'http://65.2.29.215:80/app'
+};
