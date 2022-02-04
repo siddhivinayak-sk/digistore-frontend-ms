3d2db26053f18bb6f10f750bdd00cf6f645c51fd
Sandeep Kumar <siddhivinayak.sk@gmail.com>
2022-02-04 18:01:10 +0530

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,7 +22,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'digistorefrontendms'`, () => {
+  it(`should have as title 'DigiStore Application'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('digistorefrontendms');
@@ -32,6 +32,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('digistorefrontendms app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('DigiStore Application!');
   });
 });
